f13aa8b97fedcfe2cc33f1cbf9d3cf30fb66d4e1
loogle18 <sme@jetthoughts.com>
2016-12-17 04:07:34 +0200

    Fixed bugs after rebase.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -3,6 +3,9 @@ class MetricsController < ApplicationController
   rescue_from Github::Error::NotFound || Github::Error::Unauthorized, with: :redirect_to_login
 
   def index
+    @repositories = Github.new(oauth_token: session[:token]).repos.list.reduce([]) do |arr, repo|
+      arr << repo.name
+    end
     @events = Event.all
     @labels = LabelsCarrier.new(@events)
   end
