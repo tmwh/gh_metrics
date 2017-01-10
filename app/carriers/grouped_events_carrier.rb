d2ce3c7c4c52ecdbea25f7408490ec58b1b636cf
loogle18 <sme@jetthoughts.com>
2017-01-10 18:45:22 +0200

    Added cumulating for events arrays.

--- app/carriers/grouped_events_carrier.rb
+++ app/carriers/grouped_events_carrier.rb
@@ -10,9 +10,11 @@ class GroupedEventsCarrier
 
   def collection
     uniq_labels.reduce([]) do |array, label|
-      array << WEEK.times.reduce([]) do |week, index|
-        week << events.where(created: [Date.today - index], label_name: label).count
-      end.reverse
+      events_per_day = []
+      WEEK.times.each do |index|
+        events_per_day << events.where(created: [Date.today - index], label_name: label).count
+      end
+      array << cumulate!(events_per_day.reverse)
     end
   end
 
@@ -29,6 +31,11 @@ class GroupedEventsCarrier
   end
 
   private
+
+  def cumulate!(array)
+    sum = 0
+    array.map{ |x| sum += x }
+  end
 
   def uniq_labels
     events.map(&:label_name).uniq
