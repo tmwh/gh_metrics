5c6e6ca90e8582dadcf6e1e007dab6d803bb709a
katlavan <20-86@bk.ru>
2017-01-17 18:44:20 +0200

    Added setup options

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,12 @@
 //= require materialize
 
 //= require_tree ./components
+
+
+$(document).ready(function () {
+  $('select').material_select();
+
+  $('.js-repositories-select').on('change', function(){
+    $(this).closest('form').submit();
+  });
+});
