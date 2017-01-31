ef88d83bcc45f0d88c0200cfdd988e76caa2e577
katlavan <20-86@bk.ru>
2017-01-31 18:32:36 +0200

    WIP UI reworked

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def svg(name, klass = '')
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+
+    return '(not found)' unless File.exists?(file_path)
+    content_tag(:i, File.read(file_path).html_safe, class: "svg-icon #{klass}")
+  end
 end
